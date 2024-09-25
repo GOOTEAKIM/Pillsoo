d48f8f360b46952b240b7a1548246b2de7c367f5
김구태 <gootae0220@naver.com>
2024-09-25 16:12:38 +0900

    [BE] comment : 주석 삭제

--- data_transfer/transfer_redis_to_mongo.py
+++ data_transfer/transfer_redis_to_mongo.py
@@ -16,28 +16,6 @@ mongo_client = MongoClient("mongodb://localhost:27017/")
 mongo_db = mongo_client["sos_db"]  # 데이터베이스 이름
 mongo_collection = mongo_db["recommended"]  # 컬렉션 이름
 
-'''
-def transfer_redis_to_mongodb():
-    # Redis에서 모든 키 가져오기
-    keys = r.keys()
-    if not keys:
-        print("Redis에 저장된 데이터가 없습니다.")
-        return
-    
-    for key in keys:
-        value = r.get(key)
-        if value:
-            # MongoDB에 저장
-            mongo_collection.insert_one({"client_text": key.decode(), "result": value.decode()})
-            # Redis에서 해당 키 삭제
-            r.delete(key)
-            print(f"Key {key.decode()}가 MongoDB로 이동되고 Redis에서 삭제되었습니다.")
-    print(f"총 {len(keys)}개의 키가 MongoDB로 이동되었습니다.")
-
-if __name__ == "__main__":
-    transfer_redis_to_mongodb()
-'''
-
 def transfer_redis_to_mongodb():
     # Redis에서 모든 키 가져오기
     keys = r.keys()
